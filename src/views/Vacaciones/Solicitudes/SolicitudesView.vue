1f9227e149920b5f05cd5cf8c309e5a2b81eb7c4
LXRDSZN <22680124@cuautla.tecnm.mx>
2025-10-31 17:37:52 -0600

    Historial Solicitudes --->  vacaciones

---
+++ src/views/Vacaciones/Solicitudes/SolicitudesView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import sidebar from '@/components/Barra-Navegacion/sidebar.vue';
+import SolicitudesComponent from '@/components/Vacaciones/Solicitud-vacaciones-component/Solicitudes-component.vue';
+</script>
+<template>
+  <div class="solicitudes-view">
+    <sidebar/>
+    <SolicitudesComponent/>
+  </div>
+</template>
+<style scoped>
+.solicitudes-view {
+  display: flex;
+  min-height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  background-color: #d8d8d8;
+  box-sizing: border-box;
+}
+</style>
